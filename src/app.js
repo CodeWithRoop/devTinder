bcb1b8d7372d937cd64b46daeb1186db2b61ac4f
Roopa Marigoudar <roopabm36@gmail.com>
2024-11-26 20:27:45 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app=express();
 
-app.use("/hi",(req,res)=>{res.send("hello from the server!hi roopa")});
+app.use("/test",(req,res)=>{res.send("USE handler tested!order matters in routing")});
 
-app.use("/test",(req,res)=>{res.send("hello from the server test!")});
+app.get("/test",(req,res)=>{res.send("get API tested")});
+app.post("/test",(req,res)=>{res.send("post API tested!")});
+app.delete("/test",(req,res)=>{res.send("delete API tested!")});
 
-app.use("/hello",(req,res)=>{res.send("hello from the server hello!")});
+
 app.listen(3002,()=>{
     console.log("server is successfully listening on port 3002...");
 });
