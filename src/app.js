c81a4c100a269452bfe955bfce41d557fe1b8b92
Roopa Marigoudar <roopabm36@gmail.com>
2024-11-27 08:20:10 +0530

    multiple route handler,use of next()

--- src/app.js
+++ src/app.js
@@ -1,18 +1,43 @@
 const express = require("express");
 const app=express();
 
+//mutiple route handling
+app.use("/user",
+    (req,res,next)=>{
+        console.log("handling the route user!!");  //request handler
+        next();
+    },
+    (req,res,next)=>{
+        console.log("handling the route user1!!");
+        next();
+    },
+
+
+    (req,res,next)=>{
+        console.log("handling the route user2!!");
+        next();
+    },
+
+    (req,res,next)=>{
+        console.log("handling the route user4!!");
+        res.send("user3 response");
+    }
+
+);
+
+    
 //app.use("/test",(req,res)=>{res.send("USE handler tested!order matters in routing")});
 
-app.get("/test",(req,res)=>{res.send("get API tested")});
-app.post("/test",(req,res)=>{res.send("post API tested!")});
-app.delete("/test",(req,res)=>{res.send("delete API tested!")});
+//app.get("/test",(req,res)=>{res.send("get API tested")});
+//app.post("/test",(req,res)=>{res.send("post API tested!")});
+//app.delete("/test",(req,res)=>{res.send("delete API tested!")});
 
-app.get("/test1/:userID/:userPassword",(req,res)=>{
+//app.get("/test1/:userID/:userPassword",(req,res)=>{
     //console.log(req.query);
 
     //dynamic routing :userId:password
-    console.log(req.params);
-    res.send("get API tested")});
+    //console.log(req.params);
+    //res.send("get API tested")});
 
 
 
