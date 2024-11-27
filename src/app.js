af9c2b837cf2a639cb15d0189f71723979a8705d
Roopa Marigoudar <roopabm36@gmail.com>
2024-11-27 07:33:25 +0530

    exploring routing,rejex,dynamic routing,request query,orddr of routing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app=express();
 
-app.use("/test",(req,res)=>{res.send("USE handler tested!order matters in routing")});
+//app.use("/test",(req,res)=>{res.send("USE handler tested!order matters in routing")});
 
 app.get("/test",(req,res)=>{res.send("get API tested")});
 app.post("/test",(req,res)=>{res.send("post API tested!")});
 app.delete("/test",(req,res)=>{res.send("delete API tested!")});
+
+app.get("/test1/:userID/:userPassword",(req,res)=>{
+    //console.log(req.query);
+
+    //dynamic routing :userId:password
+    console.log(req.params);
+    res.send("get API tested")});
+
 
 
 app.listen(3002,()=>{
