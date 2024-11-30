fe5b6a7214570bb2836727fd6e4a8d7851b5d755
Roopa Marigoudar <roopabm36@gmail.com>
2024-11-30 08:01:34 +0530

    validation,custom validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,49 @@
 const mongoose=require("mongoose");
 const userSchema = new mongoose.Schema({
-    firstName:{type:String,},
-    lastName:{type:String,},
-    age:{type:Number,},
-    emailId:{type:String,},
-    passWord:{type:String,},
-    gender:{type:String,}
+    firstName:{
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:50,
+    },
+    lastName:{
+        type:String,
+    },
+    age:{
+        type:Number,
+        required:true,
+        min:18,
 
-});
+    },
+    emailId:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        unique:true,
+    },
+    passWord:{
+        type:String,
+        required:true,
+    },
+    gender:{
+        type:String,
+        required:true,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("data not updated"+err.message)
+            }
+        }
+    },
+    about:{
+        type:String,
+        default:"this is the default data when about field is empty"
+    },
+    skill:{
+        type:[String],
+
+    },
+
+},
+{timestamps:true,});
 module.exports=mongoose.model("User",userSchema);
